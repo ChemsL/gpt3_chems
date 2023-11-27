02c43f6366f31666dd839bcf8ea0a8f0638e35be
unknown <chems.ly@hotmail.fr>
2023-11-27 09:36:08 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Blog, Header, Footer, Features, WhatGPT, Possibility} from './containers'
 import { Cta, Brand, Navbar } from './components'
+import './App.css'
 const App = () => {
   return (
     <div className='App'>
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
         </div>
